051056f8c9c49805f99d6066a79b7cee66c42fc7
Oscar Gimenez <ogimenezb@gmail.com>
2014-07-30 13:46:35 +0200

    Created First Admin with Products

---
+++ src/CB/WarehouseBundle/Admin/ProductAdmin.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace CB\WarehouseBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+
+class ProductAdmin extends Admin
+{
+    // Fields to be shown on create/edit forms
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->add('name')
+            ->add('description')
+            ->add('shortDesc')
+            ->add('lotRequiredInReception', null, array(
+                'required' => false,
+            ))
+            ->add('lotRequiredInExpedition', null, array(
+                'required' => false,
+            ))
+            ->add('snRequiredInReception', null, array(
+                'required' => false,
+            ))
+            ->add('snRequiredInExpedition', null, array(
+                'required' => false,
+            ))
+            ->add('active', null, array(
+                'required' => false,
+            ))
+            ->add('createdDate')
+            ->add('updatedDate')
+            ->add('baseUnit')
+        ;
+    }
+
+    // Fields to be shown on filter forms
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('name')
+            ->add('description')
+        ;
+    }
+
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->addIdentifier('name')
+            ->add('description')
+        ;
+    }
+} 
